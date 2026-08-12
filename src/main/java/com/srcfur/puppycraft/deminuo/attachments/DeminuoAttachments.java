810f39b19c1f9e6560a22863330d0d8392afd9f2
Rezzy <74584623+srcfur@users.noreply.github.com>
2026-08-12 01:49:55 -0400

    Added diaper roller kinda

---
+++ src/main/java/com/srcfur/puppycraft/deminuo/attachments/DeminuoAttachments.java
@@ -0,0 +1,15 @@
+package com.srcfur.puppycraft.deminuo.attachments;
+
+import com.mojang.serialization.Codec;
+import com.srcfur.badhygiene.api.HygieneIntegerStreamCodec;
+import com.srcfur.puppycraft.attachments.PuppyCraftAttachments;
+import net.minecraft.network.codec.StreamCodec;
+import net.neoforged.neoforge.attachment.AttachmentType;
+import net.neoforged.neoforge.network.codec.NeoForgeStreamCodecs;
+
+import java.util.function.Supplier;
+
+public class DeminuoAttachments {
+    public static final Supplier<AttachmentType<Integer>> MATURITY = PuppyCraftAttachments.ATTACHMENTS
+            .register("mental_maturity", ()-> AttachmentType.<Integer>builder(()->18).serialize(Codec.intRange(0,20)).sync(new HygieneIntegerStreamCodec()).build());
+}
