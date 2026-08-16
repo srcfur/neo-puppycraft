7bbea270b54996fb0c6afc6be305a114978032ad
Rezzy <74584623+srcfur@users.noreply.github.com>
2026-08-15 22:48:06 -0400

    Conditionally removed player full bladder slowdown

--- src/main/java/com/srcfur/puppycraft/compat/BadHygieneIntegration.java
+++ src/main/java/com/srcfur/puppycraft/compat/BadHygieneIntegration.java
@@ -1,8 +1,12 @@
 package com.srcfur.puppycraft.compat;
 
 import com.srcfur.badhygiene.data.HygieneDataTypes;
+import com.srcfur.badhygiene.events.PlayerFullBladderEvent;
 import com.srcfur.badhygiene.events.PlayerMessingEvent;
+import com.srcfur.puppycraft.Config;
 import com.srcfur.puppycraft.PuppyCraft;
+import com.srcfur.puppycraft.attachments.PuppyCraftAttachments;
+import com.srcfur.puppycraft.deminuo.attachments.DeminuoAttachments;
 import net.minecraft.world.item.ItemStack;
 import net.neoforged.bus.api.SubscribeEvent;
 
@@ -15,5 +19,9 @@ public class BadHygieneIntegration {
         if(diaper.isEmpty() || diaper.get().getOrDefault(HygieneDataTypes.HYGIENE_SOILED_CLOTHING, false)) return;
         diaper.get().set(HygieneDataTypes.HYGIENE_SOILED_CLOTHING, true);
         event.setCanceled(true);
+    }
+    @SubscribeEvent
+    public void CanPlayerBeSlowedByUrineNeed(PlayerFullBladderEvent event) {
+        event.setCanceled(PuppyCraft.getDiaperOnPlayer(event.getEntity()).isPresent() || (event.getEntity().getData(DeminuoAttachments.MATURITY.get()) < 8000 && Config.ENABLED_DEMINUO.getAsBoolean()));
     }
 }
